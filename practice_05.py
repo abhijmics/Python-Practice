6776dead54c98450e986db7463b3fa8606aace8d
abarnwal <abarnwal@zscaler.com>
2021-08-10 16:49:33 +0530

    variable type in python

---
+++ practice_05.py
@@ -0,0 +1,23 @@
+a = 5
+
+print(type(a))
+
+
+b = 5.10
+
+print(type(b))
+
+c = "Testing String"
+
+print(type(c))
+
+d = False
+
+print(type(d))
+
+e = None
+
+print(type(e))
+
+
+print(6+float('6'))
